7d65d77c60e04e0e14245bcf69b18338699edc2e
leocavalheiro10 <105437927+leocavalheiro10@users.noreply.github.com>
2023-11-08 11:28:16 -0300

    ok

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+//criando/declarando a função tocasom com parâmetro idElementoAudio
+function tocasom( idElementoAudio ) {
+    document.querySelector( idElementoAudio).play() ;
+}
+//criando a referência constante listadeTeclas
+const listaDeTeclas = document.querySelectorAll('.tecla');
+//criando referência variável contador, atribuindo o valor inicial como "0"
 
+//criando o código de repetição Wile
+//enquanto o contador for menos que o comprimento da lista, tocará o som.
+for(let contador= 0;)
